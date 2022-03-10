5d5a881faf7a7d72165741a0d0aa9d4e7e720a28
wzjustuslim <weizhengjustus.lim@gmail.com>
2022-03-10 18:30:31 +0800

    init profile accordion

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,11 @@
 import * as React from "react";
 import DashboardContent from "../layouts/DashBoard";
+import ProfileAccordions from "../components/ProfileAccordion";
 
 const title = "Profile";
 
-const render = "This is from profile page.";
+// const render = "This is from profile page.";
 
 export default function ProfilePage() {
-  return <DashboardContent title={title} render={render} />;
+  return <DashboardContent title={title} render={<ProfileAccordions />} />;
 }
