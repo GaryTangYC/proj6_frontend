7e2df5c3634c99b072fef02ab063e4d4ef86b781
wzjustuslim <weizhengjustus.lim@gmail.com>
2022-03-11 15:17:52 +0800

    deconflict profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,7 @@
 /* widget/component imports */
 import DashboardContent from "../layouts/DashBoard";
-import ProfileAccordions from "../components/ProfileAccordion";
+import ProfileAccordions from "../components/profile/ProfileAccordion";
 
-<<<<<<< HEAD
-const title = "Profile";
-
-// const render = "This is from profile page.";
-
-// ajax call to backend and store data object then pass as prop to
-// use context to store data
-
-=======
->>>>>>> d039b22d137477ae29f5977068e337b8aa0f113a
 export default function ProfilePage() {
   return (
     <DashboardContent title="Profile">
