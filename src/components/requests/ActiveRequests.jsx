68f35a83a5f9f1dfeee1991c3676e7b9d1872135
bw <bwceemail@gmail.com>
2022-03-15 12:09:22 +0800

    updated requests pagelayout - added tabbed pages - contents of tabbed pages in ./components/requests

---
+++ src/components/requests/ActiveRequests.jsx
@@ -0,0 +1,10 @@
+/* mui imports */
+import { Typography } from "@mui/material";
+
+export default function ActiveRequests() {
+  return (
+    <div>
+    <Typography variant="body1">This is active requests page</Typography>
+    </div>
+  );
+}
---
+++ src/components/requests/PendingRequests.jsx
@@ -0,0 +1,10 @@
+/* mui imports */
+import { Typography } from "@mui/material";
+
+export default function PendingRequests() {
+  return (
+    <div>
+    <Typography variant="body1">This is pending requests page</Typography>
+    </div>
+  );
+}
