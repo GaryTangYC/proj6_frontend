c804c430753be6ad47fa1b453c3a2b39ab62dead
wzjustuslim <weizhengjustus.lim@gmail.com>
2022-03-18 01:37:07 +0800

    init stripe api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-/* from docs: StrictMode is a tool for highlighting potential problems in an application. Like Fragment , StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants. Note: Strict mode checks are run in development mode only; they do not impact the production build. */
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
+(async () => {
+  const publishableKey =
+    "pk_test_51KeGEbIBxjmqX6vG78Y4dJpnCts9gA4ZQr9UlH6fX3mQ8Mtf3iRx5gspbBME50uXCuM7OXVnrq7F7JSQkog9Z33p0085Yz4YKW";
+  const stripePromise = await loadStripe(publishableKey);
+
+  /* from docs: StrictMode is a tool for highlighting potential problems in an application. Like Fragment , StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants. Note: Strict mode checks are run in development mode only; they do not impact the production build. */
+  ReactDOM.render(
+    <React.StrictMode>
+      <Elements stripe={stripePromise}>
+        <App />
+      </Elements>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+})();
