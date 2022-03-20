628ee155984be1ab3c2f31e699fe45d9f28f9572
GaryTangYC <88428535+GaryTangYC@users.noreply.github.com>
2022-03-21 00:23:00 +0800

    Working code for expiry task

--- src/components/home/TaskWithNoPartner.jsx
+++ src/components/home/TaskWithNoPartner.jsx
@@ -7,13 +7,10 @@ import { Typography } from "@mui/material";
 import TaskCardComponent from "./TaskCardComponent";
 import { Grid, Container } from "@mui/material";
 import {
-  format,
-  isValid,
-  parse,
   parseISO,
-  isPast,
   isBefore,
   isSameDay,
+  differenceInSeconds,
 } from "date-fns";
 
 export default function TaskWithPartner() {
@@ -30,7 +27,7 @@ export default function TaskWithPartner() {
   let noPartnerData = storeData.filter(
     (task) => task.partner === null && task.completed === false
   );
-  console.log("storeData", storeData);
+
   console.log("noPartnerData stored data", noPartnerData);
   let currentDateTime = new Date();
   let noPartnerExpiredData = [];
@@ -44,35 +41,39 @@ export default function TaskWithPartner() {
       new Date()
     );
 
-    // Check if task completion date is past existing date and if date is same day to be moved to expired 
+    // Check if task completion date is past existing date and if date is same day to be moved to expired
     const isDateBefore = isBefore(new Date(convertDate), new Date());
     const isDateSameDay = isSameDay(new Date(convertDate), new Date());
-    console.log("task noPartnerData", noPartnerData[i].description);
-    console.log(
-      "🚀 ~ file: TaskWithNoPartner.jsx ~ line 32 ~ TaskWithPartner ~ convertDate ",
-      convertDate
-    );
-    console.log(
-      "🚀 ~ file: TaskWithNoPartner.jsx ~ line 35 ~ TaskWithPartner ~ isDateBefore ",
-      isDateBefore
-    );
-    console.log(
-      "🚀 ~ file: TaskWithNoPartner.jsx ~ line 36 ~ TaskWithPartner ~ isDateSameDay",
-      isDateSameDay
-    );
-
-    if (isDateBefore === true && isDateSameDay === false) {
-      noPartnerExpiredData.push(noPartnerData[i]);
-      console.log("noPartnerExpiredData", noPartnerExpiredData);
-      noPartnerData.splice(i, 1);
-    }
+    const isTaskTimeExpired = differenceInSeconds(
+      new Date(convertDate),
+      new Date());
+    // console.log("task noPartnerData", noPartnerData[i].description);
+    // console.log(
+    //   "🚀 ~ file: TaskWithNoPartner.jsx ~ line 32 ~ TaskWithPartner ~ convertDate ",
+    //   convertDate
+    // );
+    // console.log(
+    //   "🚀 ~ file: TaskWithNoPartner.jsx ~ line 35 ~ TaskWithPartner ~ isDateBefore ",
+    //   isDateBefore
+    // );
+    // console.log(
+    //   "🚀 ~ file: TaskWithNoPartner.jsx ~ line 36 ~ TaskWithPartner ~ isDateSameDay",
+    //   isDateSameDay
+    // );
+      console.log('istaskTimeexpired', isTaskTimeExpired)
     
-    // For date that are same day, check on time completion to see if expired
-    if (isDateSameDay === true) {
-      testSameDateData.push(noPartnerData[i]);
+
+    // if (isDateBefore === true && isDateSameDay === false) {
+    //   noPartnerExpiredData.push(noPartnerData[i]);
+    //   console.log("noPartnerExpiredData", noPartnerExpiredData);
+    //   noPartnerData.splice(i, 1);
+    // }
+
+    if ( isTaskTimeExpired < 0) {
+    noPartnerExpiredData.push(noPartnerData[i]);
+    noPartnerData.splice(i, 1);
     }
 
-    // console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 26 ~ TaskWithPartner ~ currentDateTime ", currentDateTime )
   }
   console.log("noPartnerExpiredData", noPartnerExpiredData);
   console.log(
@@ -83,9 +84,7 @@ export default function TaskWithPartner() {
     "🚀 ~ file: TaskWithNoPartner.jsx ~ line 44 ~ TaskWithPartner ~ testSameDateData",
     testSameDateData
   );
-  // 1st for loop: Compare if task completion date isPast existing date; result is true .pop data from array to new array of expired
 
-  //2nd if: Compare if task completion time is expired (need to check which format to use) as compared to existing time); result if true .pop data from storeData to new array
 
   //Load separate array data into the required field
   console.log("this is filter task with no partner", filteredTasks);
@@ -106,23 +105,3 @@ export default function TaskWithPartner() {
     </>
   );
 }
-
-// // Extract year, month, day of completion to compare between current date
-// const year = convertDate.toISOString().substring(0,4)
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 31 ~ TaskWithPartner ~ year", year)
-// const month = convertDate.toISOString().substring(5,7)
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 32 ~ TaskWithPartner ~ month", month)
-// const day = convertDate.toISOString().substring(8,10)
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 35 ~ TaskWithPartner ~ day", day)
-// const compareDate = isPast(new Date(year, month, day))
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 40 ~ TaskWithPartner ~ compareDate", compareDate)
-// const dummyCompare = isPast(new Date(convertDate))
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 43 ~ TaskWithPartner ~ dummyCompare", dummyCompare)
-
-// const convertDate2 = convertDate.toISOString().substring(0,10)
-// console.log("🚀 ~ file: TaskWithNoPartner.jsx ~ line 36 ~ TaskWithPartner ~ convertDate2", convertDate2)
-// const checkDate = isValid(convertDate)
-// console.log('convertDate' ,convertDate)
-
-// console.log('checkDate' ,checkDate)
-// console.log('currentdatetime', currentDateTime)
